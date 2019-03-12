1c2edc026f72a4a34ef1494a4c9964388f27dd11
sdgsystems <sdgsystems@outlook.com>
2019-03-12 16:40:29 -0500

    End of Secion 5

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
    this.authService.login(this.model).subscribe(next => {
       console.log('Success!');
    }, error => {
-      console.log('Oops');
+      console.log(error);
    });
 
   }
